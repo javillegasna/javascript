e2dad2c10f4699761ebe8b2405cee0ad3588ab44
javillegasna <javillegasna@gmail.com>
2022-01-04 19:41:28 -0500

    first task

--- a-flecha/src/main.js
+++ a-flecha/src/main.js
@@ -1,21 +1,32 @@
 document.getElementById("button").onclick = () => {
     setBackgroundColorById("paragraph", "blue");
 }
+
 document.getElementById("alert").onclick = () =>{
     alert(document.getElementById("popup-input").value);
 }
+
 document.getElementById("hover-this").onmouseover = ()=>{
     setBackgroundColorById("body", "red");
 }
+
 document.getElementById("hover-this").onmouseout = ()=>{
     setBackgroundColorById("body", "#253142");
 }
+
+document.getElementById("last-p").onclick = () =>{
+    document.getElementById("last-p").style= `background-color: ${document.getElementById("color-input").value}`;
+}
+
 const getValueFromId = (id) =>{
     return document.getElementById(id).value;
 }
+
 const setBackgroundColorById = (id, color)=>{
     document.getElementById(id).style = "background-color: " + color;
 }
+
 const mouseOverFunction = (el) => {
     el.style = "background-color: black";
 }
+
